b701a1f495a3cfd591687481483a4991f8a6acee
tdevere <anthony_devere@hotmail.com>
2020-05-21 18:55:44 -0500

    come on now!

--- app/appcenter-post-clone.sh
+++ app/appcenter-post-clone.sh
@@ -5,11 +5,13 @@ echo $InAppCenter
 echo $buildVariant
 
 APPCENTER_ANDROID_VARIANT=$APPCENTER_ANDROID_VARIANT
-buildVariant=$product1Release
+buildVariantUpdate=$product1Release
 
 echo APPCENTER_ANDROID_VARIANT
 echo InAppCenter
-echo buildVariant
+echo $buildVariantUpdate
+
+set APPCENTER_ANDROID_VARIANT $buildVariantUpdate
 
 echo buildVariant
 if [ $InAppCenter == "1" ]
