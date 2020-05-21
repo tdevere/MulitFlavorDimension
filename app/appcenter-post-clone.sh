dee92b4aa53c87ad1579b02f870bff35716ec87c
tdevere <anthony_devere@hotmail.com>
2020-05-21 18:31:57 -0500

    debugging work

--- app/appcenter-post-clone.sh
+++ app/appcenter-post-clone.sh
@@ -1,19 +1,20 @@
 #!/usr/bin/env bash
 InAppCenter=$InAppCenter
 buildVariant=$buildVariant
-echo InAppCenter
-echo buildVariant
+
+find ./ -name gradlew
+ls
+printenv
+
 if [ -e "InAppCenter" ]
 then
   echo "InAppCenter"
   if [ -e "buildVariant" ]
   then
     echo "buildVariant"
-    ls
     gradlew_file= find ./ -name gradlew
     echo "gradlew_file"
     sed -i "s/$args0/$buildVariant" gradlew_file
-
   fi
 fi
 
