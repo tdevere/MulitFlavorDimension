6f4527c4cbb9c4dc522797ce1ad3d47df8c260cd
tdevere <anthony_devere@hotmail.com>
2020-05-21 20:18:00 -0500

    sed -i ''  's/$args[0]*/"'$buildVariant'"/' gradlew

--- app/appcenter-post-clone.sh
+++ app/appcenter-post-clone.sh
@@ -3,17 +3,21 @@
 cd $APPCENTER_SOURCE_DIRECTORY
 ls
 echo "read gradlew"
-cat gradlew
+#cat gradlew
 
 echo "(((((((((((((((((((((((((((((((((((updating gradlew)))))))))))))))))))))))))))))))))))"
 
 if [ $InAppCenter == "1" ]
 then
   echo "(((((((((((((((((((((((((((((((((((InAppCenter)))))))))))))))))))))))))))))))))))"
-  echo "(((((((((((((((((((((((((((((((((((updating gradlew)))))))))))))))))))))))))))))))))))"
-  sed -i ''  's/$args[0]*/"'$buildVariant'"/' gradlew
-  echo "(((((((((((((((((((((((((((((((((((updated gradlew - read gradlew)))))))))))))))))))))))))))))))))))"
-  cat gradlew
+  #echo "(((((((((((((((((((((((((((((((((((updating gradlew)))))))))))))))))))))))))))))))))))"
+  #sed -i ''  's/$args[0]*/"'$buildVariant'"/' gradlew
+  #echo "(((((((((((((((((((((((((((((((((((updated gradlew - read gradlew)))))))))))))))))))))))))))))))))))"
+  #cat gradlew
+
+  export APPCENTER_ANDROID_VARIANT=$buildVariant
+  echo $APPCENTER_ANDROID_VARIANT
+
 fi
 
 
