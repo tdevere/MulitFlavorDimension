a8338842056e181629a8dc3a80bfc77a0a4e2dde
tdevere <anthony_devere@hotmail.com>
2020-05-21 19:06:55 -0500

    sed -i

--- app/appcenter-post-clone.sh
+++ app/appcenter-post-clone.sh
@@ -1,21 +1,10 @@
 #!/usr/bin/env bash
 
-echo $APPCENTER_ANDROID_VARIANT
-echo $InAppCenter
-echo $buildVariant
-
-APPCENTER_ANDROID_VARIANT=$APPCENTER_ANDROID_VARIANT
-buildVariantUpdate=$product1Release
-
-
 if [ $InAppCenter == "1" ]
 then
   echo "InAppCenter"
-  if [ -e "buildVariant" ]
-  then
     echo "buildVariant"
-    find ./ -name gradlew -exec sed -i "s/$args0/$buildVariant" gradlew_file
-  fi
+    find ./ -name gradlew -exec sed -i "s/$args0/$buildVariant" {} +
 fi
 
 find ./ -name gradlew -exec cat {} +
