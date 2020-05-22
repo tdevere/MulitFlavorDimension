2ecad51bd651d5600ecbcc0b916acfb6f34edf91
tdevere <anthony_devere@hotmail.com>
2020-05-21 19:24:41 -0500

    this could be the one

--- app/appcenter-post-clone.sh
+++ app/appcenter-post-clone.sh
@@ -3,15 +3,16 @@
 cd $APPCENTER_SOURCE_DIRECTORY
 ls
 echo "read gradlew"
-
 cat gradlew
+
+echo "(((((((((((((((((((((((((((((((((((updating gradlew)))))))))))))))))))))))))))))))))))"
 
 if [ $InAppCenter == "1" ]
 then
-  echo "InAppCenter"
-    echo "updating gradlew"
-    sed -i 's/$args0/$buildVariant' {} +
-    echo "updated gradlew - read gradlew"
+  echo "(((((((((((((((((((((((((((((((((((InAppCenter)))))))))))))))))))))))))))))))))))"
+    echo "(((((((((((((((((((((((((((((((((((updating gradlew)))))))))))))))))))))))))))))))))))"
+    sed -i 's/$args0/$buildVariant' gradlew
+    echo "(((((((((((((((((((((((((((((((((((updated gradlew - read gradlew)))))))))))))))))))))))))))))))))))"
     cat gradlew
 fi
 
