306211fea1f3253431465c02f26a7fd777eaeefd
tdevere <anthony_devere@hotmail.com>
2020-05-21 19:17:46 -0500

    sed -i

--- app/appcenter-post-clone.sh
+++ app/appcenter-post-clone.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 echo "read gradlew"
-find ./ -name gradlew -exec cat {} +
+cat gradlew
 
 if [ $InAppCenter == "1" ]
 then
   echo "InAppCenter"
     echo "updating gradlew"
-    find ./ -name gradlew -exec sed -i 's/$args0/$buildVariant' {} +
+    sed -i 's/$args0/$buildVariant' {} +
     echo "updated gradlew - read gradlew"
-    find ./ -name gradlew -exec cat {} +
+    cat gradlew
 fi
 
 
